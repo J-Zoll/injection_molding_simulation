62831ca2af5ed9bbba2a1cf122bed04ad07e52b8
J-Zoll <jonas.zoll@student.kit.edu>
2022-02-04 17:20:35 +0100

    Refactor and add new distance function

--- src/data_processing/preprocessing.py
+++ src/data_processing/preprocessing.py
@@ -1,14 +1,13 @@
-from typing import Iterable, Tuple
-from data_processing import cleaning
-from data_processing.util import binary_aggregate, componentwise_distance, discretize
-from torch import Tensor
-import os
-import pickle
+from typing import Iterable, Tuple, List
+from util import binary_aggregate, elementwise_distance, discretize, distance
 from scipy.spatial import KDTree
 
 
-def calculate_fill_states(step_size: float, fill_times: Iterable[float]) -> Iterable[Iterable[bool]]:
-    """Calculates binary fill_states for a list of continuos node
+def calculate_fill_states(
+        step_size: float,
+        fill_times: Iterable[float]
+) -> Iterable[Iterable[bool]]:
+    """Calculates binary fill_states for a list of continuous node
        fill_times.
     """
     disc_fts = discretize(step_size, fill_times)
@@ -16,7 +15,10 @@ def calculate_fill_states(step_size: float, fill_times: Iterable[float]) -> Iter
     return ft_states
     
 
-def calculate_edges(node_positions: Iterable[Tuple[float, float, float]], connection_range: float) -> Iterable[Tuple[int, int]]:
+def calculate_edges(
+        node_positions: Iterable[Tuple[float, float, float]],
+        connection_range: float
+) -> Iterable[Tuple[int, int]]:
     """Calculates edges. Nodes are connected if their distance is
        smaller or equal to connection_range"""
     kd_tree = KDTree(node_positions)
@@ -34,19 +36,35 @@ def calculate_edges(node_positions: Iterable[Tuple[float, float, float]], connec
     return edges
 
 
-def calculate_distances(node_positions: Iterable[Tuple[float, float, float]], edges: Iterable[Tuple[int, int]] ) -> Iterable[Tuple[float, float, float]]:
-    """Calculates the edge-distances for a list of edges."""
+def calculate_elementwise_distances(
+        node_positions: List[List[float]],
+        edges: Iterable[Tuple[int, int]]
+) -> List[List[float]]:
+    """Calculates the element-wise distances between connected nodes"""
     distances = []
     for i, j in edges:
         p1 = node_positions[i]
         p2 = node_positions[j]
-        distances.append(componentwise_distance(p1, p2))
+        distances.append(elementwise_distance(p1, p2))
     return distances
 
 
-def encode_fill_state(fill_states: Iterable[bool]) -> Iterable[Tuple[float, float]]:
-    """Encodes the fill_state trough one-hot encoding."""
-    FILLED = [1.0, 0.0]
-    NOT_FILLED = [0.0, 1.0]
+def calculate_distances(
+        node_positions: List[Tuple[float, float, float]],
+        edges: Iterable[Tuple[int, int]]
+) -> List[float]:
+    """Calculates the euclidean distances between connected nodes"""
+    distances = []
+    for i, j in edges:
+        p1 = node_positions[i]
+        p2 = node_positions[j]
+        distances.append(distance(p1, p2))
+    return distances
 
-    return [FILLED if fs else NOT_FILLED for fs in fill_states]
+
+def encode_fill_state(fill_states: Iterable[bool]) -> List[Tuple[float, float]]:
+    """Encodes the fill_state as one-hot encoding."""
+    enc_filled = (1.0, 0.0)
+    enc_not_filled = (0.0, 1.0)
+
+    return [enc_filled if fs else enc_not_filled for fs in fill_states]
--- src/data_processing/test_preprocessing.py
+++ src/data_processing/test_preprocessing.py
@@ -1,14 +1,15 @@
 import unittest
 import preprocessing
 
-class Test_calculate_edges(unittest.TestCase):
+
+class TestPreprocessing(unittest.TestCase):
     def test_calculates_edges(self):
         node_positions = [
-            [0,0,0],
-            [1,0,0],
-            [0,1,0]
+            [0, 0, 0],
+            [1, 0, 0],
+            [0, 1, 0]
         ]
-        connection_range=1.0
+        connection_range = 1.0
         target_edges = list({
             (0, 1),
             (0, 2)
@@ -19,16 +20,12 @@ class Test_calculate_edges(unittest.TestCase):
         )
         self.assertEqual(edges, target_edges)
 
-
-class Test_encode_fill_state(unittest.TestCase):
     def test_encodes_fill_state(self):
         fill_states = [False, True]
-        target_enc_fill_states = [[0.0, 1.0], [1.0, 0.0]]
+        target_enc_fill_states = [(0.0, 1.0), (1.0, 0.0)]
         enc_fill_states = preprocessing.encode_fill_state(fill_states)
         self.assertEqual(enc_fill_states, target_enc_fill_states)
 
-
-class Test_calculate_fill_states(unittest.TestCase):
     def test_calculates_fill_states(self):
         step_size = .2
         fill_times = [.0, .1, .2, .3]
