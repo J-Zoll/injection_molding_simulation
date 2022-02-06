21c73e4dbe83aeb139286efa91c11fbde7751cc3
J-Zoll <jonas.zoll@student.kit.edu>
2022-02-06 14:21:22 +0100

    Outsource processing logic from dataset to preprocessing module

--- src/dataset.py
+++ src/dataset.py
@@ -1,4 +1,4 @@
-from torch_geometric.data import Dataset, Data
+from torch_geometric.data import Dataset
 import pandas as pd
 import os
 from tqdm import tqdm
@@ -6,6 +6,7 @@ from typing import Union, List, Tuple
 from data_processing import preprocessing
 from data_processing.util import parse_to_float_list
 from torch import LongTensor, Tensor
+import shutil
 import torch
 
 
@@ -33,48 +34,15 @@ class InjectionMoldingDataset(Dataset):
         pass
 
     def process(self):
-        data_counter = 0
-        for path_to_raw_study_data in tqdm(self.raw_paths, desc="process studies"):
-            df_study = pd.read_csv(f"{path_to_raw_study_data}.csv")
+        self.clear_processed_dir()
 
-            node_positions = [parse_to_float_list(p) for p in df_study.position]
-            
-            # calculate edges and edge_distances
-            edge_list = preprocessing.calculate_edges(node_positions, self.connection_range)
-            edge_index = LongTensor(edge_list).T
-            elementwise_distances = preprocessing.calculate_elementwise_distances(node_positions, edge_list)
-            distances = preprocessing.calculate_distances(node_positions, edge_list)
-
-            # calculate fill_states
-            fill_states = preprocessing.calculate_fill_states(self.time_step_size, df_study.fill_time)
-
-            for t, _ in enumerate(fill_states[:-1]):
-                # fill_state at the beginning of the time step
-                old_fs = Tensor(preprocessing.encode_fill_state(fill_states[t]))
-                node_attributes = old_fs
-
-                # (prediction goal) fill_state at the end of the time step
-                new_fs = Tensor(preprocessing.encode_fill_state(fill_states[t + 1]))
-                target_node_attributes = new_fs
-
-                edge_attributes = Tensor(elementwise_distances)
-                edge_weight = Tensor(distances)
-
-                node_positions = Tensor(node_positions)
-
-                # create data object
-                data = Data(
-                    x=node_attributes,
-                    edge_index=edge_index,
-                    edge_attr=edge_attributes,
-                    edge_weight=edge_weight,
-                    y=target_node_attributes,
-                    pos=node_positions,
-                    study=path_to_raw_study_data,
-                    time=t * self.time_step_size
-                )
-                torch.save(data, os.path.join(self.processed_dir, f"data_{data_counter}.pt"))
-                data_counter += 1
+        for raw_file_path in tqdm(self.raw_paths, desc="process studies"):
+            preprocessing.process_raw_file(
+                raw_file_path,
+                self.processed_dir,
+                self.connection_range,
+                self.time_step_size
+            )
 
     def len(self):
         file_names = os.listdir(self.processed_dir)
@@ -83,3 +51,21 @@ class InjectionMoldingDataset(Dataset):
 
     def get(self, idx):
         return torch.load(os.path.join(self.processed_dir, f'data_{idx}.pt'))
+
+    def clear_processed_dir(self):
+        """Removes all files that get generated during processing"""
+        print("Clearing processed_dir..")
+
+        # list files to delete
+        files_to_delete = ["pre_filter.pt", "pre_transform.pt"]
+        for fn in os.listdir(self.processed_dir):
+            if fn.startswith("data"):
+                files_to_delete.append(fn)
+
+        # delete files
+        for fn in files_to_delete:
+            fp = os.path.join(self.processed_dir, fn)
+            if os.path.isfile(fp):
+                os.remove(fp)
+
+        print("Finished clearing!")
